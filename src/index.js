c8a0016656d10556d4860236e150dee28e2b1e33
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-06-28 20:21:39 +0300

    ,

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ const { v4: uuidv4 } = require('uuid');
 
 const used = uuidv4();
 console.log(used);
+import { alert, defaultModules } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+text: 'Notice me, senpai!',
+});

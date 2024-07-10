2d937201a59d21db352992ab4a2cb98c3102adc9
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-07-10 20:30:16 +0300

    ю

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-const used = uuidv4();
-console.log(used);
-import { alert, defaultModules } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import * as PNotifyMobile from '@pnotify/mobile';
-import '@pnotify/mobile/dist/PNotifyMobile.css';
+// const used = uuidv4();
+// console.log(used);
+// import { alert, defaultModules } from '@pnotify/core';
+// import '@pnotify/core/dist/PNotify.css';
+// import * as PNotifyMobile from '@pnotify/mobile';
+// import '@pnotify/mobile/dist/PNotifyMobile.css';
 
-defaultModules.set(PNotifyMobile, {});
+// defaultModules.set(PNotifyMobile, {});
 
-alert({
-text: 'Notice me, senpai!',
-});
+// alert({
+// text: 'Notice me, senpai!',
+// });
 
+
+import colors from "./colors.json"
+import colorsTmp1 from './templates/colors.handlebars'
+console.log(colorsTmp1);
 

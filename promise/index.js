e979f971c902684ad3c92316e2432bbdf02395c8
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-08-23 19:15:50 +0300

    ,

--- promise/index.js
+++ promise/index.js
@@ -21,26 +21,84 @@
 //непарне, проміс повинен бути відхилено з повідомленням 
 //"Є непарні числа".
 
-function arrChecking (arr) {
-    const everyNum = arr.every((num) => {
-     return   num % 2 === 0
-    })
-    return new Promise((resolve, reject) => {
+// function arrChecking (arr) {
+//     const everyNum = arr.every((num) => {
+//      return   num % 2 === 0
+//     })
+//     return new Promise((resolve, reject) => {
         
-        if (everyNum) {
-            resolve('всі числа парні')
-        } else {
-            reject('Є непарні числа')
+//         if (everyNum) {
+//             resolve('всі числа парні')
+//         } else {
+//             reject('Є непарні числа')
 
-        }
-    })
-}
+//         }
+//     })
+// }
 
-arrChecking([2, 4, 6])
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+// arrChecking([2, 4, 6])
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
    
+// const isOpenModal = true
+
+// const promise1 = new Promise((resolve, reject) => {
+//     if (isOpenModal) {
+//         setTimeout(() => {
+//                 resolve('promise1 виконався успішно')
+//             }, 1000)
+//     } else {
+//         reject('promise1 не виконався')
+//         }
+// })
+    
+// const promise2 = new Promise((resolve, reject) => {
+//   if (isOpenModal) {
+//         setTimeout(() => {
+//                 resolve('promise2 виконався успішно')
+//             }, 2000)
+//     } else {
+//         reject('promise2 не виконався')
+//         }
+// })
+    
+// const promise3 = new Promise((resolve, reject) => {
+//    if (isOpenModal) {
+//         setTimeout(() => {
+//                 resolve('promise3 виконався успішно')
+//             }, 3000)
+//     } else {
+//         reject('promise3 не виконався')
+//         }
+// })
+    
+// console.dir(Promise)
+// Promise.all([promise1, promise2, promise3])
+//     .then((response) => {
+//         console.log(response);
+//     }
+// )
+//     .catch(error=>console.log(error));
+    
+
+
+
+const promise1 = new Promise((resolve, reject) =>{
+    setTimeout(()=>{resolve(console.log('проміс 1'))}, 5000)
+})
+const promise2 = new Promise((resolve, reject) =>{
+    setTimeout(()=>{resolve(console.log('проміс 2'))}, 8000)
+})
+const promise3 = new Promise((resolve, reject) =>{
+    setTimeout(()=>{resolve(console.log('проміс 3'))}, 1000)
+})
+
+
+Promise.rase([promise1, promise2, promise3])
+    .then((first) => {
+    return console.log(first);
+})
